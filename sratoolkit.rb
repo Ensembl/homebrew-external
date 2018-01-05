917166335ad7864245ac10d2329fa5b220b56049
Andrew Yates <ayates@ebi.ac.uk>
2018-01-05 15:50:23 +0000

    Bringing in sratoolkit as latest version does not work

---
+++ sratoolkit.rb
@@ -0,0 +1,84 @@
+class Sratoolkit < Formula
+  desc "Data tools for INSDC Sequence Read Archive"
+  homepage "https://github.com/ncbi/sra-tools"
+  # doi "10.1093/nar/gkq1019"
+  # tag "bioinformatics"
+  # tag origin homebrew-science
+  # tag derived
+
+  url "https://github.com/ncbi/sra-tools/archive/2.8.2-1.tar.gz"
+  version "2.8.2-1"
+  sha256 "0f28313c648c0dfad885376e403369f3110a1f21ce47b26a215aec5d65d455b1"
+  revision 1
+
+  head "https://github.com/ncbi/sra-tools.git"
+
+  depends_on "autoconf" => :build
+  depends_on "libxml2"
+  depends_on "libmagic" => :recommended
+  depends_on "hdf5" => :recommended
+
+  resource "ngs-sdk" do
+    url "https://github.com/ncbi/ngs/archive/1.3.0.tar.gz"
+    sha256 "803c650a6de5bb38231d9ced7587f3ab788b415cac04b0ef4152546b18713ef2"
+  end
+
+  resource "ncbi-vdb" do
+    url "https://github.com/ncbi/ncbi-vdb/archive/2.8.2.tar.gz"
+    sha256 "15c9a47a9ebaf01822c567817acd681c4a594b4ac92e8bcf08d4d580f62296a9"
+  end
+
+  def install
+    ENV.deparallelize
+
+    # Linux fix: libbz2.a(blocksort.o): relocation R_X86_64_32 against `.rodata.str1.1'
+    # https://github.com/Homebrew/homebrew-science/issues/2338
+    ENV["LDFLAGS"]="" if OS.linux?
+
+    resource("ngs-sdk").stage do
+      cd "ngs-sdk" do
+        system "./configure", "--prefix=#{prefix}",
+                              "--build=#{Pathname.pwd}/ngs-sdk-build"
+        system "make"
+        system "make", "test"
+        system "make", "install"
+      end
+    end
+
+    (buildpath/"ncbi-vdb").install resource("ncbi-vdb")
+    cd "ncbi-vdb" do
+      system "./configure", "--prefix=#{prefix}",
+                            "--with-ngs-sdk-prefix=#{prefix}",
+                            "--build=#{buildpath}/ncbi-vdb-build"
+      system "make"
+      system "make", "install"
+    end
+
+    inreplace "tools/copycat/Makefile", "-smagic-static", "-smagic"
+
+    # Fix the error: undefined reference to `SZ_encoder_enabled'
+    inreplace "tools/pacbio-load/Makefile", "-shdf5 ", "-shdf5 -ssz "
+
+    system "./configure", "--prefix=#{prefix}",
+                          "--with-ngs-sdk-prefix=#{prefix}",
+                          "--with-ncbi-vdb-sources=#{buildpath}/ncbi-vdb",
+                          "--with-ncbi-vdb-build=#{buildpath}/ncbi-vdb-build",
+                          "--build=#{buildpath}/sra-tools-build"
+
+    system "make", "VDB_SRCDIR=#{buildpath}/ncbi-vdb"
+    system "make", "VDB_SRCDIR=#{buildpath}/ncbi-vdb", "install"
+
+    rm "#{bin}/magic"
+    rm_rf "#{bin}/ncbi"
+    rm_rf "#{lib}64"
+    rm_rf include.to_s
+
+    pkgshare.install share/"examples"
+  end
+
+  test do
+    # just download the first FASTQ read from an NCBI SRA run (needs internet connection)
+    system bin/"fastq-dump", "-N", "1", "-X", "1", "SRR000001"
+    assert_match "@SRR000001.1 EM7LVYS02FOYNU length=284", File.read("SRR000001.fastq")
+  end
+end
