f9a1c12a324ddd3e5f6790314d14d165b6df7947
Andrew Yates <andrewyatz@users.noreply.github.com>
2018-07-09 16:36:51 +0100

    Update to version 0.4.2

--- libbigwig.rb
+++ libbigwig.rb
@@ -1,7 +1,7 @@
 class Libbigwig < Formula
   desc "C library for processing the big UCSC fomats"
   homepage "https://github.com/dpryan79/libBigWig"
-  url "https://github.com/dpryan79/libBigWig/archive/0.4.1.tar.gz"
+  url "https://github.com/dpryan79/libBigWig/archive/0.4.2.tar.gz"
   sha256 "0dd6f52f6c73aaad5b00265d742bdaaf12b490cd3673fac74f80bcb661d4a591"
   # tag "bioinformatics"
   # tag origin homebrew-science
