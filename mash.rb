51c6c6854340271040a1ddcb94ec5fd39aab5a49
Matthieu Muffato <muffato@ebi.ac.uk>
2019-05-09 23:54:43 +0100

    bugfix: made compatible with capnp 0.7
    See https://github.com/marbl/Mash/issues/98

--- mash.rb
+++ mash.rb
@@ -1,4 +1,5 @@
 class Mash < Formula
+  # cite Ondov_2016: "https://doi.org/10.1186/s13059-016-0997-x"
   desc "Fast genome distance estimation using MinHash"
   homepage "https://github.com/marbl/Mash"
   # tag "bioinformatics"
@@ -10,15 +11,20 @@ class Mash < Formula
   sha256 "7bea8cd3c266640bbd97f2e1c9d0168892915c1c14f7d03a9751bf7a3709dd01"
   head "https://github.com/marbl/Mash.git"
 
-  needs :cxx11
-
   depends_on "autoconf" => :build
   depends_on "automake" => :build
+  depends_on "libtool" => :build
+  depends_on "pkg-config" => :build
+
   depends_on "capnp"
   depends_on "gsl"
   depends_on "zlib" unless OS.mac?
 
   def install
+    # https://github.com/marbl/Mash/issues/98
+    inreplace "configure.ac", "c++11", "c++14"
+    inreplace "Makefile.in", "c++11", "c++14"
+
     system "./bootstrap.sh"
     system "./configure",
       "--prefix=#{prefix}",
