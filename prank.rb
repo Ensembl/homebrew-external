51b1550aa6231b8f502058f34c203593a8c41afd
Thibaut Hourlier <thibauthourlier@users.noreply.github.com>
2019-09-23 13:12:37 +0100

    Update path to file
    The file has been moved in a directory, no need to bump version or revision

--- prank.rb
+++ prank.rb
@@ -1,7 +1,7 @@
 class Prank < Formula
   desc "A multiple alignment program for DNA, codon and amino-acid sequences"
   homepage "http://wasabiapp.org/software/prank/"
-  url "http://wasabiapp.org/download/prank/prank.source.140603.tgz"
+  url "http://wasabiapp.org/download/prank/previous_version/prank.source.140603.tgz"
   sha256 "9a48064132c01b6dba1eec90279172bf6c13d96b3f1b8dd18297b1a53d17dec6"
   # tag origin homebrew-science
   # tag derived
