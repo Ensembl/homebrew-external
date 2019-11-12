68e6892fe2615a8134786d7ffdc4f4ef34f3a9b5
Matthieu Muffato <muffato@ebi.ac.uk>
2019-11-12 11:13:55 +0000

    List the moonshine dependencies

--- travisci/test_on_docker.sh
+++ travisci/test_on_docker.sh
@@ -2,7 +2,8 @@
 
 brew deps --union "$@" | if grep -q ensembl/moonshine/
 then
-    echo Test skipped because ensembl/moonshine is not available
+    echo Test skipped because the formulae rely on ensembl/moonshine, which is not available:
+    brew deps --union "$@" | grep ensembl/moonshine/
 else
     brew install --build-from-source "$@"
 fi
