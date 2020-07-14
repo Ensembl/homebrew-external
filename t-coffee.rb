295cd2b92459350e080e37010210037cb211bf32
Matthieu Muffato <muffato@ebi.ac.uk>
2020-07-14 12:19:51 +0100

    The source URL is gone. Compile from the source code instead

--- t-coffee.rb
+++ t-coffee.rb
@@ -1,8 +1,8 @@
 class TCoffee < Formula
   homepage "http://www.tcoffee.org/"
   version '9.03.r1318'
-  url "http://www.tcoffee.org/Packages/Stable/Version_9.03.r1318/T-COFFEE_distribution_Version_9.03.r1318.tar.gz"
-  sha256 "5bb9a531a4036b741a8ff0fe19f3591a3f33bf7ac4f484e5329b1b5dd1fff43c"
+  url "https://github.com/cbcrg/tcoffee/archive/27c894eee70b44e4ea13130bdead2b299e13daa9.tar.gz"
+  sha256 "c289ef0fe5a4fb534faba9ed980303f36d027799ad1d0bde8a0822a549784a75"
   # doi "10.1006/jmbi.2000.4042"
   # tag origin homebrew-science
   # tag derived
@@ -18,17 +18,22 @@ class TCoffee < Formula
   depends_on 'ensembl/external/kalign'
 
   def install
-    cd 't_coffee_source' do
-      # Fix this error: MAX_N_PID exceded -- Recompile changing the value of MAX_N_PID (current: 260000 Requested: 263956)
-      inreplace "io_lib_header.h", "define MAX_N_PID       260000", "define MAX_N_PID       520000"
-      inreplace "define_header.h", "define MAX_N_PID       260000", "define MAX_N_PID       520000"
-      system *%w[make t_coffee]
+    # Fix this error: MAX_N_PID exceded -- Recompile changing the value of MAX_N_PID (current: 260000 Requested: 263956)
+    inreplace "lib/data_headers/coffee_defines.h", "define MAX_N_PID       260000", "define MAX_N_PID       520000"
+    mkdir "lib/compilation"
+    cd "t_coffee/src" do
+      p = buildpath + "_build"
+      # The makefile copies the executable there
+      ENV["USER_BIN"] = p
+      # The makefile copies the preprocessed source files there
+      mkdir p + "distributions/T-COFFEE_distribution_Version_9.03/t_coffee_source"
+      ENV["HOME2"] = p
+      # Uncomment to see which paths it is using
+      #system "make", "env"
+      system "make", "c_code"
       bin.install 't_coffee'
-      #prefix.install "lib" => "libexec"
-      #prefix.install Dir["*"]
-      #bin.install_symlink "../compile/t_coffee"
     end
-    (prefix+'plugins').install 'mcoffee'
+    (prefix+'plugins').install 'lib/mcoffee'
     File.open((etc+'tcoffee.bash'), 'w') { |file| file.write("export MCOFFEE_4_TCOFFEE=#{prefix}/plugins/mcoffee
 ") }
   end
