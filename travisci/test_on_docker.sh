2f789f5a6d03d744d1507d9773baa554e760a58c
Matthieu Muffato <muffato@ebi.ac.uk>
2020-07-14 12:54:51 +0100

    git can't lock files on the mounted directory, so needs to work on a copy

--- travisci/test_on_docker.sh
+++ travisci/test_on_docker.sh
@@ -2,6 +2,14 @@
 
 set -euo pipefail
 
+cd /home/linuxbrew/EnsemblTaps
+for i in *
+do
+    TARGET="/home/linuxbrew/.linuxbrew/Homebrew/Library/Taps/ensembl/$i"
+    rm -rf "$TARGET"
+    cp -a "$i" "$TARGET"
+done
+
 brew deps --union "$@" | if grep -q ensembl/moonshine/
 then
     echo Test skipped because the formulae rely on ensembl/moonshine, which is not available:
