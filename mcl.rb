a70f8b2cb64bbc8f7ea7e39b838bf0bf9c2dfad4
Thibaut Hourlier <thibaut@ebi.ac.uk>
2021-08-03 15:22:42 +0100

    Change to test docker 2

--- mcl.rb
+++ mcl.rb
@@ -8,7 +8,7 @@ class Mcl < Formula
   # tag origin homebrew-science
   # tag derived
 
-  depends_on "gcc@6"
+  depends_on "gcc@6" => :build
   fails_with gcc: "7"
   fails_with gcc: "8"
   fails_with gcc: "9"
