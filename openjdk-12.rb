171864c6aa51d6b0978d8b57db1ee5f5d6216062
Thibaut Hourlier <thibaut@ebi.ac.uk>
2022-03-29 15:22:49 +0100

    Remove :unneeded flag as obsolete

--- openjdk-12.rb
+++ openjdk-12.rb
@@ -8,7 +8,6 @@ class Openjdk12 < Formula
   url "https://download.java.net/java/GA/jdk12.0.1/69cfe15208a647278a19ef0990eea691/12/GPL/openjdk-12.0.1_linux-x64_bin.tar.gz"
   sha256 "151eb4ec00f82e5e951126f572dc9116104c884d97f91be14ec11e85fc2dd626"
 
-  bottle :unneeded
   keg_only "this would clash with other JDKs (and java is selected via jenv anyway)"
 
   depends_on :linux
